25b060a144e36459028eb72da17195039143a8ee
The Bearodactyl <dabewodactowluwu@gmail.com>
2026-01-05 00:27:34 -0600

    center title

---
+++ build.lua
@@ -0,0 +1,1 @@
+os.execute("tsc script.ts --lib es2024,dom")
---
+++ script.js
@@ -0,0 +1,4 @@
+var title = document.getElementById("title");
+title === null || title === void 0 ? void 0 : title.addEventListener("click", function () {
+    console.log("woah");
+});
